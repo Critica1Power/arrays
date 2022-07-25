17ba7e7e5c1401e764abbfb4d93bb1a526bd11da
JavaDeveloper <Kozhevnikov95igor@gmail.com>
2022-07-25 18:13:10 +0300

    newbranch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) {task1();}
+
+    public static void task1() {
+
         System.out.println("Hello world!");
+
     }
 }
