7567091e06aa32b0c2b27fb9f14ac60e60d510d1
JavaDeveloper <Kozhevnikov95igor@gmail.com>
2022-08-08 22:00:39 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,19 @@ public class Main {
     public static void main(String[] args) {task1();}
 
     public static void task1() {
+        // Задание 1
 
-        System.out.println("Hello world!");
+        int[] array1 = new int[5];
+        array1[0] = 1;
+        array1[1] = 2;
+        array1[2] = 3;
+
+        double[] array2 = new double[] {1.57, 7.654, 9.986};
+
+        long[] array3 = new long[3];
+        array3[0] = 567;
+        array3[1] = 8372;
+        array3[2] = 26904;
 
     }
 }
